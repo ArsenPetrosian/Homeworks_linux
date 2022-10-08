383902f2530b4ebf6ec7577a184aac68d71243a9
ArsenPetrosian <ars.petrosyan03@gmail.com>
2022-10-08 15:07:31 +0400

    added Homework_4

---
+++ Homework_4/exercise/CMakeFiles/Calculate_average.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculate_average.dir/Calculate_average.cpp.o"
+  "CMakeFiles/Calculate_average.dir/Calculate_average.cpp.o.d"
+  "Calculate_average"
+  "Calculate_average.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calculate_average.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
