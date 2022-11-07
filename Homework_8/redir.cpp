8037e2ed4f29841509a452f11d2a05f6e7460edb
ArsenPetrosian <ars.petrosyan03@gmail.com>
2022-11-07 17:10:57 +0400

    changed Homework_8

--- Homework_8/redir.cpp
+++ Homework_8/redir.cpp
@@ -21,65 +21,47 @@ int main(int argc, char** argv)
         int out_file{};
         if(strcmp(argv[2], "-args") == 0)
         {
-            bool out_a = false;
-            bool err_a = false;
             int program_argc{};
-
-            if(strcmp(argv[argc - 2], "-a") == 0)
+            int i = 3;
+            while(strcmp(argv[i], "-out") != 0)
             {
-                err_file = open(argv[argc - 1], flags, 0666);
-                err_a = true;
-                if(strcmp(argv[argc - 4], "-a") == 0)
+                ++program_argc;
+                ++i;
+            }
+            if(strcmp(argv[i + 1], "-a") == 0)
+            {
+                out_file = open(argv[i + 2], flags, 0666);
+                lseek(out_file, 0, SEEK_END);
+                if(strcmp(argv[i + 4], "-a") == 0)
                 {
-                    out_a = true;
-                        program_argc = argc - 5 - 2;
+                    err_file = open(argv[i + 5], flags, 0666);
+                    lseek(err_file, 0, SEEK_END);
                 }
                 else
-                    program_argc = argc - 4 - 2;
+                    err_file = open(argv[i + 4], flags |  O_TRUNC, 0666);
             }
             else
             {
-                err_file = open(argv[argc - 1], flags | O_TRUNC, 0666);
-                if(strcmp(argv[argc - 3], "-a") == 0)
+                out_file = open(argv[i + 1], flags | O_TRUNC, 0666);
+                if(strcmp(argv[i + 3], "-a") == 0)
                 {
-                    out_a = true;
-                    program_argc = argc - 4 - 2;
+                    err_file = open(argv[i + 4], flags, 0666);
+                    lseek(err_file, 0, SEEK_END);
                 }
                 else
-                    program_argc = argc - 3 - 2;
-            }
-
-            if(err_a)
-            {
-                lseek(err_file, 0, SEEK_END);
-                if(out_a)
-                {
-                    out_file = open(argv[argc - 3], flags, 0666);
-                    lseek(out_file, 0, SEEK_END);
-                }
-                else
-                    out_file = open(argv[argc - 3], flags | O_TRUNC, 0666);
-            }
-            else
-            {
-               if(out_a)
-                {
-                    out_file = open(argv[argc - 2], flags, 0666);
-                    lseek(out_file, 0, SEEK_END);
-                }
-                else
-                    out_file = open(argv[argc - 2], flags | O_TRUNC, 0666); 
+                    err_file = open(argv[i + 3], flags | O_TRUNC, 0666);
             }
             if (out_file < 0 || err_file < 0)
             {
                 perror("Error occured while trying to open the file.");
                 return 1;
             }
-
+            
             char* program_argv[program_argc + 1];
-            for(int i = 0; i < program_argc; ++i)
-                program_argv[i] = argv[i + 2];
+            for(int j = 0; j < program_argc; ++j)
+                program_argv[j] = argv[j + 3];
             program_argv[program_argc] = NULL;
+            
             dup2 (out_file, STDOUT_FILENO);
             close(out_file);
             dup2 (err_file, STDERR_FILENO);
@@ -93,28 +75,28 @@ int main(int argc, char** argv)
         }
         else
         {
-            if(strcmp(argv[argc - 2], "-a") == 0)
+            if(strcmp(argv[3], "-a") == 0)
             {
-                err_file = open(argv[argc - 1], flags, 0666);
-                lseek(err_file, 0, SEEK_END);
-                if(strcmp(argv[argc - 4], "-a") == 0)
+                out_file = open(argv[4], flags, 0666);
+                lseek(out_file, 0, SEEK_END);
+                if(strcmp(argv[6], "-a") == 0)
                 {
-                    out_file = open(argv[argc - 3], flags, 0666);
-                    lseek(out_file, 0, SEEK_END);
+                    err_file = open(argv[7], flags, 0666);
+                    lseek(err_file, 0, SEEK_END);
                 }
                 else
-                    out_file = open(argv[argc - 3], flags | O_TRUNC, 0666);
+                    err_file = open(argv[6], flags | O_TRUNC, 0666);
             }
             else
             {
-                err_file = open(argv[argc - 1], flags | O_TRUNC, 0666);
-                if(strcmp(argv[argc - 3], "-a") == 0)
+                out_file = open(argv[3], flags | O_TRUNC, 0666);
+                if(strcmp(argv[5], "-a") == 0)
                 {
-                    out_file = open(argv[argc - 2], flags, 0666);
-                    lseek(out_file, 0, SEEK_END);
+                    err_file = open(argv[6], flags, 0666);
+                    lseek(err_file, 0, SEEK_END);
                 }
                 else
-                    out_file = open(argv[argc - 2], flags | O_TRUNC, 0666);
+                    err_file = open(argv[5], flags | O_TRUNC, 0666);
             }
             if (out_file < 0 || err_file < 0)
             {
@@ -128,6 +110,7 @@ int main(int argc, char** argv)
             close(out_file);
             dup2 (err_file, STDERR_FILENO);
             close(err_file);
+            
             if (execvp(argv[1], program_argv) < 0)
             {
                 perror("Program execution failed.");
