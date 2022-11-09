3a0ac89b9306e49046e964404c01f5d7ccdd2475
ArsenPetrosian <ars.petrosyan03@gmail.com>
2022-11-09 15:22:49 +0400

    added build directories

---
+++ Homework_4/build/CMakeFiles/calculate_average.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/arsen/Homeworks_linux/Homework_4/calculate_average.cpp" "CMakeFiles/calculate_average.dir/calculate_average.cpp.o" "gcc" "CMakeFiles/calculate_average.dir/calculate_average.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
